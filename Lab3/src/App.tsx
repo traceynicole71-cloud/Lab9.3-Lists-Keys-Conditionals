636544ac7726d9015f721978b35e6c71fbbd8580
Tracey Roberts <traceynicole71@yahoo.com>
2026-04-24 11:48:28 -0400

    create add task feature

--- Lab3/src/App.tsx
+++ Lab3/src/App.tsx
@@ -13,22 +13,43 @@ function App() {
   function handleStatusChange(id: string, status: TaskStatus) {
     setTasks(prev => prev.map(t => t.id === id ? { ...t, status } : t));
   }
-
+//handle delete
 const handleDelete = (id: string) => {
   setTasks(prev => prev.filter(t => t.id !== id));
 };
-
+//filter tasks
+const [filter, setFilter] = useState<TaskStatus | 'all'>('all');
 const filteredTasks = task.filter(task => {
-  return true;
+  return filter === 'all' || task.status === filter;
 });
 
+//handle filter change
+const handleFilterChange = (filters: { status?: TaskStatus; priority?: string }) => {
+  if (filters.status) {
+    setFilter(filters.status);
+  }
+};
+
+//add tasks
+const handleAddTask = (priority: 'low' | 'medium' | 'high' ) => {
+  const newTask: Task = {
+    id: crypto.randomUUID(),
+    title: `Task ${task.length + 1}`,
+    description: "New task details",
+    status: 'pending',
+    priority: priority,
+    dueDate: new Date().toISOString().split('T')[0]
+  };
+  setTasks(prev => [newTask, ...prev]);
+};
   return (
-   <div>
-    <TaskFilter onFilterChange={(f) => console.log(f)} />
+   <div className="App">
+    <TaskFilter onFilterChange={handleFilterChange} />
       <TaskList
       tasks={filteredTasks}
 onStatusChange={handleStatusChange}
 onDelete={handleDelete}
+onAddTask={handleAddTask}
 />
    </div>
   );
--- Lab3/src/components/TaskList/TaskList.tsx
+++ Lab3/src/components/TaskList/TaskList.tsx
@@ -3,14 +3,23 @@ import type { TaskListProps } from '../../types/index.ts'
 import TaskItem from '../TaskItem/TaskItem.tsx'
 
 //conditional for empty states
-const TaskList: React.FC<TaskListProps> = ({ tasks, onStatusChange, onDelete }) => {
+const TaskList: React.FC<TaskListProps> = ({ tasks, onStatusChange, onDelete, onAddTask }) => {
 
     if (tasks.length === 0) {
         return <p>No Tasks Found.</p>;
     }
 
 return (
-    <div className="task-list">
+    <div className="task-list-container">
+        <div style={{ marginBottom: '20px', textAlign: 'center' }}>
+            <button
+            onClick={onAddTask}
+            style={{ padding: '10px 20px', cursor: 'pointer' }}
+            >
+                + Add New Task
+            </button>
+        </div>
+        
         {tasks.map((task) => (
             <TaskItem
             key={task.id}
